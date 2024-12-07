149109524e6eca80e8911faa85bfcd4091943f97
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2024-12-07 14:56:16 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 
   return (
     <ClerkProvider
-      publishableKey="pk_test_cHJvcGVyLXJvZGVudC01MS5jbGVyay5hY2NvdW50cy5kZXYk"
+      publishableKey={process.env.CLERK_PUBLISHABLE_KEY}
       appearance={{
         variables: { colorPrimary: "#624cf5" },
       }}
